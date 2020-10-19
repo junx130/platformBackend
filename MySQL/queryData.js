5beb6c02b5027635d27b3017c6a5ad643749e652
junxian <junx130@gmail.com>
2020-10-19 17:54:50 +0800

    apiNAfterT

--- MySQL/queryData.js
+++ MySQL/queryData.js
@@ -1,9 +1,6 @@
 
 const { pool } = require("./db");
 const database = "RawDataLog";
-// const moment = require("moment");
-// var { DateTime } = require('luxon');
-
 
 async function getNMinData(devType, devID, minute){
     const sqlQuery = `SELECT * FROM (SELECT * FROM Device_${devType}_${devID} WHERE timestamp >= now() - INTERVAL ${minute} MINUTE)Var1 ORDER BY unix DESC ;`
@@ -40,32 +37,55 @@ async function getLastNData(devType, devID, qty){
 }
 
 async function getDataT1ToT2(devType, devID, T1, T2){
-  // process.env.TZ = "Asia/Kuala_Lumpur";  
-  // let d = DateTime.local();
-  // console.log(d.zoneName); //Asia/Saigon
-  // process.env.TZ = 'Asia/Kuwait';
-  // let tNow = new Date();
-  // let offset = tNow.getTimezoneOffset();
-  // console.log("tz offset:", tNow.getTimezoneOffset());  
-  // console.log((offset>=0?"+":"-")+parseInt(offset/60)+":"+offset%60);
-  // console.log("T1: ", sT1);
-  // console.log("T2: ", sT2);
   const sqlQuery = `SELECT * FROM Device_${devType}_${devID} WHERE unix >= '${T1}' AND unix <= '${T2}'`;
   let connection;
   try {
     connection = await pool.getConnection();
     result = await connection.query(`use ${database}`);
     result = await connection.query(sqlQuery);
-    // console.log("Create DB", result);
     return result;
   } catch (ex) {
     console.log("DB Error", ex.message);
   } finally {
     if (connection) connection.end();
-    console.log("Get Last N Data Finally");
+    console.log("T to T Finally");
   }
 }
 
+async function getNDataAfterT(devType, devID, T1, n1){
+  const sqlQuery = `SELECT * FROM Device_${devType}_${devID} WHERE unix >= '${T1}' limit ${n1}`;
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    result = await connection.query(`use ${database}`);
+    result = await connection.query(sqlQuery);
+    return result;
+  } catch (ex) {
+    console.log("DB Error", ex.message);
+  } finally {
+    if (connection) connection.end();
+    console.log("N After T Finally");
+  }
+}
+
+async function getNMinAfterT(devType, devID, T1, min1){
+  const sqlQuery = `SELECT * FROM Device_${devType}_${devID} WHERE unix >= '${T1}' AND unix <= '${T1}' + ${min1}*60 `;
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    result = await connection.query(`use ${database}`);
+    result = await connection.query(sqlQuery);
+    return result;
+  } catch (ex) {
+    console.log("DB Error", ex.message);
+  } finally {
+    if (connection) connection.end();
+    console.log("N Min After T Finally");
+  }
+}
+
+exports.getNMinAfterT = getNMinAfterT;
+exports.getNDataAfterT = getNDataAfterT;
 exports.getDataT1ToT2 = getDataT1ToT2;
 exports.getNMinData =getNMinData;
 exports.getLastNData =getLastNData;
--- Routes/queryData.js
+++ Routes/queryData.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getLastNData, getNMinData, getDataT1ToT2 } = require("../MySQL/queryData");
+const { getLastNData, getNMinData, getDataT1ToT2, getNDataAfterT, getNMinAfterT } = require("../MySQL/queryData");
 const router = express.Router();
 
 router.get("/min/:ty/:id/:minute", async (req, res) => {
@@ -16,6 +16,18 @@ router.get("/n/:ty/:id/:qty", async (req, res) => {
 
 router.get("/ttot/:ty/:id/:t1/:t2", async (req, res) => {
     let result= await getDataT1ToT2(req.params.ty, req.params.id, req.params.t1, req.params.t2);
+    if (!result) return res.status(400).send("Invalid request");
+    return res.status(200).send(result);
+});
+
+router.get("/naft/:ty/:id/:t1/:n1", async (req, res) => {
+    let result= await getNDataAfterT(req.params.ty, req.params.id, req.params.t1, req.params.n1);
+    if (!result) return res.status(400).send("Invalid request");
+    return res.status(200).send(result);
+});
+
+router.get("/minaft/:ty/:id/:t1/:min1", async (req, res) => {
+    let result= await getNMinAfterT(req.params.ty, req.params.id, req.params.t1, req.params.min1);
     if (!result) return res.status(400).send("Invalid request");
     return res.status(200).send(result);
 });
