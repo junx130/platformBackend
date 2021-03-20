2e8ef87082798b57ae35750836ebcb7d3ef25b20
junxian <junx130@gmail.com>
2021-03-20 18:19:56 +0800

    jxMonListBackUpCommit

--- Routes/ControlDevice/ctrlDevRoute.js
+++ Routes/ControlDevice/ctrlDevRoute.js
@@ -8,30 +8,35 @@ let httpResponse;
 let subTopic;
 let nodeType;
 let _nodeID;
+let nodeFunction;
 
 function clearCommonVar(){
     subTopic = undefined;
     nodeType = undefined;
     _nodeID = undefined;
+    nodeFunction = undefined;
 }
 
 expClient.on("message", async (topic, message) => {
     // console.log("Message Lai Liao");
     const a_topic = topic.split("/");
     // console.log(a_topic);
-    if(a_topic[0]===`Aploud` &&
+    if(a_topic[0]===`Aplouds` &&
         a_topic[1]===`NodeReply` ){
             // console.log("Topic match");
             let deviceInfo = JSON.parse(message);
-            // console.log(deviceInfo);
+            console.log(deviceInfo);
             // console.log(nodeType);
             // console.log(deviceInfo.DevId);
             // console.log(deviceInfo.NodeType);
             // console.log(_nodeID);
-            if(nodeType && _nodeID && deviceInfo.NodeType == nodeType && deviceInfo.DevId == _nodeID){
+            if(nodeType && _nodeID && nodeFunction && 
+                deviceInfo.ht == nodeType && deviceInfo.hi == _nodeID && 
+                deviceInfo.hd == 3 && deviceInfo.hf == nodeFunction ){
                 if(httpResponse){
-                    // console.log("Res valid");                
+                    console.log(deviceInfo);                
                     httpResponse.status(200).send(deviceInfo);
+                    console.log(`Success, Unsubcribe ${subTopic}`);                
                     if(subTopic) unsubscribeTopic(subTopic);
                     clearTimeout(timer);
                     clearCommonVar();
@@ -47,18 +52,19 @@ router.post("/send", auth, async (req, res) => {
         httpResponse = res;
         console.log(req.body);      
         // console.log(currentNo);                
-        publishMqtt(`Aplouds/BEtoGW/${req.body.ht}`, req.body);
-        nodeType = req.body.nodeType;
-        _nodeID = req.body.nodeId;
-        // console.log("KittyMeow");
+        publishMqtt(`Aplouds/ServerToNode/${req.body.gwid}`, req.body);
+        nodeType = req.body.ht;
+        _nodeID = req.body.hi;
+        nodeFunction = req.body.hf;
         // console.log(req.body.nodeId);
         // console.log(_nodeID);
-        subTopic = `Aploud/NodeReply`;
+        subTopic = `Aplouds/NodeReply/${nodeType}/${_nodeID}`;
         subscribeTopic(subTopic);       /**subscribe topic here*/
 
         timer = setTimeout(function(){
             //   resp.send(array);
             // console.log("After 5 sec");
+            console.log(`Unsubcribe ${subTopic}`);     
             if(subTopic) unsubscribeTopic(subTopic);     /** unsubscribe topic here*/
             clearCommonVar();
             
