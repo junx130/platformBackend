0aace3c0ff6727ee373964db9f10d28f13c4c03d
junxian <junx130@gmail.com>
2022-02-25 18:04:25 +0800

    pendingDefaultBdSubList

--- MySQL/V2_Action/V2_Tele.js
+++ MySQL/V2_Action/V2_Tele.js
@@ -4,6 +4,7 @@ const db = "V2_Action";
 
 const contactTable = 'V2_tele_contactList';
 const groupTable = 'V2_tele_groupList';
+const contactUnderGroupTable = 'V2_tele_contactUnderGroup';
 
 async function insertTeleContactList(contactInfo, user_id) {    
     const createTable = `CREATE TABLE IF NOT EXISTS ${contactTable}(	
@@ -128,9 +129,71 @@ async function updateContactList(info, user_id, _id){
     }
 }
 
+
+async function insertGroupTable(groupName, user_id) {    
+    try {
+        const createTable = `CREATE TABLE IF NOT EXISTS ${groupTable}(	
+            _id int NOT NULL AUTO_INCREMENT,
+            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            unix INT(11) NOT NULL,
+            name varchar(40),
+            addByUser_id int,
+            inUse tinyint default 1,
+            PRIMARY KEY (_id)
+        );`;
+    
+        const insertData = `INSERT INTO ${groupTable} (unix, name, addByUser_id)
+        VALUES (UNIX_TIMESTAMP(), "${groupName}", ${user_id});`;
+        // console.log("insertData: ", insertData);
+        
+        let result = await insertTemplate(db, createTable, insertData, "insertGroupTable");
+        // console.log("insertFormula rel: ", result);
+        if(!result) return false    // insert error
+        if(result.affectedRows > 0 && result.insertId > 0) return {success:true, insertId:result.insertId}
+        // console.log("Insert Error");
+        return false;   // insert no row effec
+    } catch (error) {
+        console.log("insertGroupTable Err : ", error.message);
+        return false
+    }
+    
+}
+
+async function insertContactUnderGroup(group_id, contact_id, user_id) {
+    try {
+        const createTable = `CREATE TABLE IF NOT EXISTS ${contactUnderGroupTable}(	
+            _id int NOT NULL AUTO_INCREMENT,
+            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            unix INT(11) NOT NULL,
+            teleGroup_id int,
+            teleContact_id int,
+            addByUser_id int,
+            inUse tinyint default 1,
+            PRIMARY KEY (_id)
+        );`;
+    
+        const insertData = `INSERT INTO ${contactUnderGroupTable} (unix, teleGroup_id, teleContact_id, addByUser_id)
+        VALUES (UNIX_TIMESTAMP(), ${group_id}, ${contact_id}, ${user_id});`;
+        // console.log("insertData: ", insertData);
+        
+        let result = await insertTemplate(db, createTable, insertData, "insertContactUnderGroup");
+        // console.log("insertFormula rel: ", result);
+        if(!result) return false    // insert error
+        if(result.affectedRows > 0 && result.insertId > 0) return {success:true, insertId:result.insertId}
+        // console.log("Insert Error");
+        return false;   // insert no row effec
+    } catch (error) {
+        console.log("insertContactUnderGroup Err : ", error.message);
+        return false
+    }
+    
+}
+
 exports.updateContactList=updateContactList;
 exports.insertTeleContactList =insertTeleContactList;
 exports.checkName_ChatID_duplicated=checkName_ChatID_duplicated;
 exports.getSingleNotInuse=getSingleNotInuse;
 exports.getInuseContactbyUser_id=getInuseContactbyUser_id;
 exports.getInuseGroupbyUser_id=getInuseGroupbyUser_id;
+exports.insertGroupTable=insertGroupTable;
+exports.insertContactUnderGroup=insertContactUnderGroup;
--- Routes/V2_Action/TeleRoute/V2_Act_TeleRoute.js
+++ Routes/V2_Action/TeleRoute/V2_Act_TeleRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../../Middleware/auth");
-const { checkName_ChatID_duplicated, getSingleNotInuse, insertTeleContactList, updateContactList, getInuseGroupbyUser_id, getInuseContactbyUser_id } = require("../../../MySQL/V2_Action/V2_Tele");
+const { checkName_ChatID_duplicated, getSingleNotInuse, insertTeleContactList, updateContactList, getInuseGroupbyUser_id, getInuseContactbyUser_id, insertGroupTable, insertContactUnderGroup } = require("../../../MySQL/V2_Action/V2_Tele");
 const { notArrOrEmptyArr } = require("../../../utilities/validateFn");
 
 router.post("/insertcontact", auth, async (req, res) => {    
@@ -46,15 +46,51 @@ router.post("/getsubscriber", auth, async (req, res) => {
         let {user_id} = req.user;
         /** query contact */
         let sub_contact = await getInuseContactbyUser_id(user_id)
-        console.log("sub_contact", sub_contact);
+        // console.log("sub_contact", sub_contact);
 
         /** query group list */
         let sub_group0 = await getInuseGroupbyUser_id(user_id)
-        console.log("sub_group0", sub_group0);
+        // console.log("sub_group0", sub_group0);
 
         /** send 2 list to user */
         return res.status(200).send({success:true, contactList:sub_contact, groupList:sub_group0});    
 
+
+    } catch (error) {
+        console.log("getsubscriber : ", error.message);
+        return res.status(203).send({errMsg: "Database Error (Exp)"});        
+    }
+});
+
+router.post("/insertnewgroup", auth, async (req, res) => {    
+    try {
+        let body = req.body;
+        let {user_id} = req.user;
+        console.log("body", body);
+
+        /** check group got same group name */
+        let groupList = await getInuseGroupbyUser_id(user_id)
+        let nameOccupy = groupList.find(c=>c.name === body.name);
+        console.log("nameOccupy", nameOccupy);
+        if(nameOccupy) return res.status(203).send({errMsg: "Group name occupied"});        
+
+        /** insert new group, V2_tele_groupList, get _id */
+        let addGroupRel = await insertGroupTable(body.name, user_id);
+        if(!addGroupRel) return res.status(203).send({errMsg: "Create group error (DB)"});
+
+        /** insert each contact to V2_tele_contactUnderGroup, use _id above */
+        let someContactErr = false;
+        for (const eachContact of body.contactList) {
+            if(eachContact.subType !==1 ) continue
+            let insertCUnderGRel = await insertContactUnderGroup(addGroupRel.insertId, eachContact.sub_id, user_id);
+            if(!insertCUnderGRel) {
+                someContactErr=true;
+                continue
+            }            
+        }
+        if(someContactErr) return res.status(203).send({errMsg:"Some contact insert error"});
+        
+        return res.status(200).send({success:true, group_id:addGroupRel.insertId});    
 
     } catch (error) {
         console.log("getsubscriber : ", error.message);
