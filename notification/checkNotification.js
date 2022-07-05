139eb2c9937a554f711447ad2b8e73c3f0c7f412
junxian <junx130@gmail.com>
2022-07-05 18:25:09 +0800

    SkipUpdateNotifyOnLaptop

--- notification/checkNotification.js
+++ notification/checkNotification.js
@@ -315,7 +315,7 @@ async function checkNotification(bdDev){
             }
             // write into database, update NotifiedUnix to timenow    
             if(process.env.activateTelegram==="true")    {
-                if(!PDC_SkipNotification) await updateNotifiedUnix(notifyItem._id, _unixNow());
+                if(!PDC_SkipNotification && process.env.debugOnLaptop!=="true" ) await updateNotifiedUnix(notifyItem._id, _unixNow());
             }
         }
         return notifyList;
