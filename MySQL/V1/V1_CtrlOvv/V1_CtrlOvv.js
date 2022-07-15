f399710c39c4b7db0a536c24529b3fd6ae810c45
junxian <junx130@gmail.com>
2022-07-15 14:19:57 +0800

    save gateway id done

--- MySQL/V1/V1_CtrlOvv/V1_CtrlOvv.js
+++ MySQL/V1/V1_CtrlOvv/V1_CtrlOvv.js
@@ -58,7 +58,57 @@ async function get_V1_ctrlGwPair (bdDev_id){
     }
 }
 
+async function V1_insertGatewayPair(info) {
+    let fnName = "V1_insertGatewayPar";
+    try {
+        const createTable = `CREATE TABLE IF NOT EXISTS ${gwPaitTable}(	
+            _id int NOT NULL AUTO_INCREMENT,
+            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            unix INT(11) NOT NULL,
+            bdDev_id int,
+            gwid int,
+            inUse tinyint default 1,
+            PRIMARY KEY (_id)
+        );`;
+
+        const insertData = `INSERT INTO ${gwPaitTable} (unix, bdDev_id, gwid)
+        VALUES (UNIX_TIMESTAMP(), ${info.bdDev_id}, ${info.gwid});`;        
+
+        let result = await insertTemplate(db, createTable, insertData, `${fnName} Finally`);
+        if (!result) return null    // insert error
+        if (result.affectedRows > 0 && result.insertId > 0) return { success: true, insertId: result.insertId }
+
+    } catch (error){
+        console.log(`${fnName} err : `, error.message);
+        return null;
+    }
+}
+
+async function V1_updateGwPair(bdDev_id, gwid) {
+    let sMsg = "V1_updateGwPair";
+    try {
+        const quertCmd = `UPDATE ${gwPaitTable} SET 
+            unix=UNIX_TIMESTAMP(),
+            gwid = ${gwid},
+            inUse  = 1
+            where bdDev_id = ${bdDev_id}`;
+        // console.log("quertCmd", quertCmd);
+
+        let result = await queryTemplate(db, quertCmd, `${sMsg} Finally`);
+        // console.log(result);
+        if (!result || !result.affectedRows) return null;
+        if (result.affectedRows > 0) return true;
+        return null
+
+    } catch (error) {
+        console.log(`Error : ${sMsg}`, error.message);
+        return null;
+    }
+}
+
 
 exports.get_V1_ctrlOvvAreaInBd_ctrlPg=get_V1_ctrlOvvAreaInBd_ctrlPg;
 exports.get_V1_ctrlOvvDevInBd_ctrlPg=get_V1_ctrlOvvDevInBd_ctrlPg;
 exports.get_V1_ctrlGwPair=get_V1_ctrlGwPair;
+exports.V1_insertGatewayPair=V1_insertGatewayPair;
+exports.V1_updateGwPair=V1_updateGwPair;
--- Routes/V1/V1_CtrlOvv/V1_CtrlOvv.js
+++ Routes/V1/V1_CtrlOvv/V1_CtrlOvv.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../../Middleware/auth");
-const { get_V1_ctrlOvvAreaInBd_ctrlPg, get_V1_ctrlOvvDevInBd_ctrlPg, get_V1_ctrlGwPair } = require("../../../MySQL/V1/V1_CtrlOvv/V1_CtrlOvv");
+const { get_V1_ctrlOvvAreaInBd_ctrlPg, get_V1_ctrlOvvDevInBd_ctrlPg, get_V1_ctrlGwPair, V1_insertGatewayPair, V1_updateGwPair } = require("../../../MySQL/V1/V1_CtrlOvv/V1_CtrlOvv");
+const { notArrOrEmptyArr } = require("../../../utilities/validateFn");
 
 
 
@@ -30,6 +31,28 @@ router.post("/ovv/getgwpair", auth, async (req, res) => {
         return res.status(200).send({gwPairList});
     } catch (error) {
         console.log("/ovv/getgwpair : ", error.message);
+        return res.status(203).send({ errMsg: "Database Error (Exp)" });
+    }
+});
+
+router.post("/ovv/setgwpair", auth, async (req, res) => {
+    try {
+        let {bdDev_id, gwid} = req.body;
+        // console.log("bdDev_id : ", bdDev_id);
+        // console.log("gwid : ", gwid);
+        /** check gw pair exist */
+        let gwList = await get_V1_ctrlGwPair(bdDev_id);
+        if(notArrOrEmptyArr(gwList)){   /** not exist, insert*/
+            let inertRel = await V1_insertGatewayPair({bdDev_id, gwid});
+            if(!inertRel) return res.status(203).send({errMsg:"Set Gw Err (Insert failed)"});    
+        }else{      /** exist update */
+            let updaterel = await V1_updateGwPair(bdDev_id, gwid);
+            if(!updaterel) return res.status(203).send({errMsg:"Set Gw Err (Update failed)"});    
+        }
+
+        return res.status(200).send({success:true});
+    } catch (error) {
+        console.log("/ovv/setgwpair : ", error.message);
         return res.status(203).send({ errMsg: "Database Error (Exp)" });
     }
 });
